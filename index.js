0341b6765ffab63a160edf02973609d617fbf846
sirmmy0002 <sammyndungu52@gmail.com>
2022-06-16 23:45:48 +0300

    done

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word){
+  return word.split("").reverse().join("")
 }
 
-/* 
-  Add your pseudocode here
-*/
+function isPalindrome(word){
+  return word == reverseString(word)
+}
 
-/*
-  Add written explanation of your solution here
-*/
 
-// You can run `node index.js` to view these console logs
+
+
 if (require.main === module) {
-  // add your own custom tests in here
+  
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
